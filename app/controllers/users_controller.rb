bbd44264c8e8b54a9b6cf911ae412406aced22c8
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-25 16:36:20 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,9 +52,9 @@ class UsersController < ApplicationController
   end
 
   get '/users/:slug' do
-
+    binding.pry
      @user = User.find_by_slug(params[:slug])
-     binding.pry
+
      erb :'users/show'
    end
 
